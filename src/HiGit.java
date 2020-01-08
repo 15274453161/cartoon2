7bfb5f45b48aefebea78efff546334beae09a7bc
qgh <2542953433@qq.com>
2020-01-08 17:30:36 +0800

    第二次更新

--- src/HiGit.java
+++ src/HiGit.java
@@ -1,6 +1,6 @@
 public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit");
-        System.out.println("666666");
+        System.out.println("第二次更新");
     }
 }
